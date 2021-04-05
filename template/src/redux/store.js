bd7e4eb5874abd38b989cd88d98ef5056640908e
Kashish Jain <jainabhishek7204@gmail.com>
2021-04-05 11:38:20 +0530

    Removed unused boilerplate code

--- template/src/redux/store.js
+++ template/src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import mainReducer from "../features/Slice";
+import mainReducer from "./Slice";
 
 export default configureStore({
   reducer: {
     slice: mainReducer,
   },
+  devTools: !process.env.NODE_ENV || process.env.NODE_ENV === "development",
 });
